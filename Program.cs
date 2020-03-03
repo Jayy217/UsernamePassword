8320cbeee692448fd56a307a72f73f0021d167fb
Jacob Youssif <103043338@student.swin.edu.au>
2020-03-03 16:50:16 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UsernamePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sysUsername = "bob";
+            string sysPassword = "Password";
+
+            string username;
+            string password;
+
+            System.Console.WriteLine("Please enter username");
+            username = Console.ReadLine();
+
+
+            if (username == sysUsername)
+            {
+                System.Console.WriteLine("Please enter your password");
+                password = Console.ReadLine();
+
+                if (password == sysPassword)
+                {
+                    System.Console.WriteLine("Login Successful");
+                }
+                else
+                {
+                    System.Console.WriteLine("Login Unsuccessful");
+                }
+
+            }
+            else
+            {
+                System.Console.WriteLine("Login Unsuccessful");
+            }
+        }
+    }
+}
